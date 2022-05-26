44b177137644a98828f6936680e73538390b013f
Nathan Yi <nathanyi112@gmail.com>
2022-05-25 21:04:43 -0500

    adds pages and component boilerplate

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Index from '../pages/Index'
+import Show from '../pages/Show'
+
+
+export default function Main(props) {
+    return(
+        <div>
+            <h1>Main</h1>
+        </div>
+    )
+}
---
+++ src/pages/Show.jsx
@@ -0,0 +1,7 @@
+export default function Show() {
+    return(
+        <div>
+            <h1>Show Page</h1>
+        </div>
+    )
+}
